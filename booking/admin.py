b53280c5084041f4b69f746c538a255f70e65c3e
Manik Bhatia <114804863+bhatiamanik@users.noreply.github.com>
2024-09-30 21:40:12 +0530

    Update admin.py

--- booking/admin.py
+++ booking/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from booking.models import Booking, Seat, Train, TrainSchedule
 
 # Register your models here.
+
 admin.site.register(Train)
 admin.site.register(TrainSchedule)
 admin.site.register(Booking)
